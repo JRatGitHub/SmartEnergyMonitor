3ba550f3f3fd24598ced7ad85beaed4021763e60
JRatGitHub <janroelof@dewit-qian.nl>
2021-04-07 21:25:34 +0200

    Update module.php

--- P1monitor/module.php
+++ P1monitor/module.php
@@ -109,8 +109,11 @@
 			$data = file_get_contents($url); // put the contents of the file into a variable
 			$wizards = json_decode($data, true);
 			
-			SetValueFloat($this->GetIDForIdent('ROOM_TEMPERATURE_IN'),$wizards['0']['3']);
-			SetValueFloat($this->GetIDForIdent('ROOM_TEMPERATURE_OUT'),$wizards['0']['7']);
+			$bTemperatuur = $this->RegisterPropertyBoolean('temperatuur');
+			if ($bTemperatuur){
+				SetValueFloat($this->GetIDForIdent('ROOM_TEMPERATURE_IN'),$wizards['0']['3']);
+				SetValueFloat($this->GetIDForIdent('ROOM_TEMPERATURE_OUT'),$wizards['0']['7']);
+			}
 			
 			//financial
 			$url = 'http://' .$this->ReadPropertyString('IPAddress') .'/api/v1/financial/day?limit=1';
